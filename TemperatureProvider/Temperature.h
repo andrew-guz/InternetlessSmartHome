91b0f57921ccc99213c7dbcfabae45663e5d4517
Andrew Guz <ndrew.guz.spb@gmail.com>
2026-04-02 21:34:20 +0300

    Reorganize files

--- TemperatureProvider/Temperature.h
+++ TemperatureProvider/Temperature.h
@@ -1,15 +1,1 @@
-#pragma once
-
-#include <Adafruit_BMP085.h>
-
-class I2CTemperatureSensor {
-public:
-    I2CTemperatureSensor() = default;
-
-    void Setup() { _sensor.begin(); }
-
-    float GetTemperature() { return _sensor.readTemperature(); }
-
-private:
-    Adafruit_BMP085 _sensor;
-};
+../include/Temperature.h
---
+++ include/WiFi.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <IPAddress.h>
+#include <WString.h>
+#include <functional>
+
+class WiFiDataServer {
+public:
+    void Setup(const String& ssid, const String& password) {
+        WiFi.softAP(ssid, password);
+
+        _ipAddress = WiFi.softAPIP();
+
+        _server.begin();
+    }
+
+    IPAddress GetAddress() { return _ipAddress; }
+
+    struct Response {
+        int code;
+        String contentType;
+        String content;
+    };
+
+    void Register(const String& path, HTTPMethod method, std::function<WiFiDataServer::Response(void)> handler) {
+        _server.on(path, method, [this, handler]() {
+            WiFiDataServer::Response response = handler();
+            _server.send(response.code, response.contentType, response.content);
+        });
+    }
+
+    void Loop() { _server.handleClient(); }
+
+private:
+    ESP8266WebServer _server{ 80 };
+    IPAddress _ipAddress;
+};
