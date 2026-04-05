68af3ab1b25fb629eb55bcae1a4520cbebec87c5
Andrew Guz <ndrew.guz.spb@gmail.com>
2026-04-05 17:56:29 +0300

    OTA name must be different

--- include/Defines.h
+++ include/Defines.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define WIFI_NETWORK_PREFIX "ISM_"
-#define OTA_NAME_PREFIX     "ISM_"
+#define OTA_NAME_PREFIX     "ISM_OTA_"
 #define WIFI_PASSWORD       "12345678" // Must be at least 8 chars
 #define OTA_PASSWORD        "12345678"
 #define WWW_USER_NAME       "admin"
