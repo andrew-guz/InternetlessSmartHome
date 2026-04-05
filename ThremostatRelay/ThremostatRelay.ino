627604dc28c772427e552c2c3a960430f179a2e1
Andrew Guz <ndrew.guz.spb@gmail.com>
2026-04-05 20:11:57 +0300

    Renaming

--- ThremostatRelay/ThremostatRelay.ino
+++ ThremostatRelay/ThremostatRelay.ino
@@ -13,7 +13,7 @@ Memory memory;
 
 unsigned long lastUpdate = 0;
 bool manualMode = false;
-bool manualOn = false;
+bool manualState = false;
 String thermometerName = "";
 float targetTemperature = 20.0f;
 float temperatureDelta = 0.5f;
@@ -26,7 +26,7 @@ void setup() {
 
     manualMode = memory.Load("manualMode", false);
     if (manualMode) {
-        manualOn = memory.Load("manualOn", false);
+        manualState = memory.Load("manualState", false);
     }
 
     thermometerName = memory.Load("thermometerName", String{});
@@ -34,7 +34,7 @@ void setup() {
     temperatureDelta = memory.Load("temperatureDelta", 0.5f);
 
     if (manualMode) {
-        digitalWrite(RELAY_PIN, manualOn ? (ON_BY_HIGH_LEVEL ? HIGH : LOW) : (ON_BY_HIGH_LEVEL ? LOW : HIGH));
+        digitalWrite(RELAY_PIN, manualState ? (ON_BY_HIGH_LEVEL ? HIGH : LOW) : (ON_BY_HIGH_LEVEL ? LOW : HIGH));
     }
 
     server.Register("/manualMode", HTTPMethod::HTTP_GET, [&]() {
@@ -57,14 +57,14 @@ void setup() {
             .content = "OK",
         };
     });
-    server.Register("/manualOn", HTTPMethod::HTTP_GET, [&]() {
+    server.Register("/manualState", HTTPMethod::HTTP_GET, [&]() {
         return WiFiDataServer::Response{
             .code = 200,
             .contentType = "text/plain",
-            .content = manualOn ? "true" : "false",
+            .content = manualState ? "true" : "false",
         };
     });
-    server.Register("/manualOn", HTTPMethod::HTTP_POST, [&](const String& body) {
+    server.Register("/manualState", HTTPMethod::HTTP_POST, [&](const String& body) {
         if (manualMode == false) {
             return WiFiDataServer::Response{
                 .code = 300,
@@ -74,11 +74,11 @@ void setup() {
         }
 
         const bool newValue = body == "true";
-        if (newValue != manualOn) {
-            manualOn = newValue;
-            memory.Save("manualOn", manualOn);
+        if (newValue != manualState) {
+            manualState = newValue;
+            memory.Save("manualState", manualState);
 
-            digitalWrite(RELAY_PIN, manualOn ? (ON_BY_HIGH_LEVEL ? HIGH : LOW) : (ON_BY_HIGH_LEVEL ? LOW : HIGH));
+            digitalWrite(RELAY_PIN, manualState ? (ON_BY_HIGH_LEVEL ? HIGH : LOW) : (ON_BY_HIGH_LEVEL ? LOW : HIGH));
         }
 
         return WiFiDataServer::Response{
