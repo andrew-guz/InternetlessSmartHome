f04062fa3e520bb17e0b5288f90c9baab2d5de4b
Andrew Guz <ndrew.guz.spb@gmail.com>
2026-04-23 20:56:27 +0300

    Do not loose config in bash build

--- arduino-build.sh
+++ arduino-build.sh
@@ -9,13 +9,11 @@ if [ ! -f "$CONFIG_FILE" ]; then
   exit 1
 fi
 
-# fqbn
-FQBN=$(grep '"board"' "$CONFIG_FILE" | sed -E 's/.*"board": *"([^"]+)".*/\1/')
-
-# путь к ino
+FQBN_BASE=$(grep '"board"' "$CONFIG_FILE" | sed -E 's/.*"board": *"([^"]+)".*/\1/')
+CONFIG=$(grep '"configuration"' "$CONFIG_FILE" | sed -E 's/.*"configuration": *"([^"]+)".*/\1/')
 SKETCH=$(grep '"sketch"' "$CONFIG_FILE" | sed -E 's/.*"sketch": *"([^"]+)".*/\1/')
 
-if [ -z "$FQBN" ]; then
+if [ -z "$FQBN_BASE" ]; then
   echo "board not found"
   exit 1
 fi
@@ -25,7 +23,14 @@ if [ -z "$SKETCH" ]; then
   exit 1
 fi
 
-# получаем папку скетча
+if [ -n "$CONFIG" ]; then
+  FQBN="$FQBN_BASE:$CONFIG"
+else
+  FQBN="$FQBN_BASE"
+fi
+
 SKETCH_DIR=$(dirname "$SKETCH")
+
+mkdir -p build
 
 arduino-cli compile --fqbn "$FQBN" "$SKETCH_DIR" --build-path build
